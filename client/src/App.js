65d8bde828858a3bb5e2ccaa9c68c0193e15e3bc
Kwang Woo Lee <kl467@cornell.edu>
2019-12-18 09:38:00 +0900

    MVP + Stretch 1/4

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+// Import Dependencies
 import React, { useState } from 'react';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
+// Import Components
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -9,12 +15,28 @@ const App = () => {
     setSavedList( [...savedList, movie] );
   };
 
+  
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <MovieList />
+          </Route>
+          <Route path="/movies/:id">
+            <Movie />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
 
 export default App;
+
+
+{/* <nav>
+<NavLink to="/">MovieList</NavLink>
+<NavLink to="/movies/">Movie</NavLink>
+</nav> */}
